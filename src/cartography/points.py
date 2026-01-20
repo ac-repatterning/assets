4891958a62e2e0cdabcc5f2dea0bce5a950616c0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-20 16:17:28 +0000

    Update src/cartography/points.py: In Progress

--- src/cartography/points.py
+++ src/cartography/points.py
@@ -19,25 +19,21 @@ class Points:
         self.__care = care
         self.__reference = reference
 
-        # fields
-        self.__f_care = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'organisation',
-                         'town', 'local_authority', 'geometry']
-        self.__f_reference = ['catchment_id', 'catchment_name', 'focus', 'station_id', 'latitude', 'longitude',
-                              'station_name', 'ts_name', 'river_name', 'gauge_datum', 'geometry']
-
     def __get_care(self) -> geopandas.GeoDataFrame:
         """
 
         :return:
         """
 
-        care = self.__care.copy()
+        __f_care = ['catchment_id', 'catchment_name', 'focus', 'latitude', 'longitude', 'organisation',
+                    'town', 'local_authority', 'geometry']
 
+        care = self.__care.copy()
         care['latitude'] = care.geometry.apply(lambda k: k.y)
         care['longitude'] = care.geometry.apply(lambda k: k.x)
         care['focus'] = 'elders'
 
-        return care[self.__f_care]
+        return care[__f_care]
 
     def __get_reference(self) -> geopandas.GeoDataFrame:
         """
@@ -45,10 +41,13 @@ class Points:
         :return:
         """
 
+        __f_reference = ['catchment_id', 'catchment_name', 'focus', 'station_id', 'latitude', 'longitude',
+                         'station_name', 'ts_name', 'river_name', 'gauge_datum', 'geometry']
+
         reference = self.__reference.copy()
         reference['focus'] = 'gauge'
 
-        return reference[self.__f_reference]
+        return reference[__f_reference]
 
     def exc(self) -> geopandas.GeoDataFrame:
         """
@@ -57,10 +56,7 @@ class Points:
         """
 
         care = self.__get_care()
-        logging.info(care)
-
         reference = self.__get_reference()
-        logging.info(reference)
 
         # Concatenating
         data = pd.concat([care, reference], axis=0, ignore_index=True)
