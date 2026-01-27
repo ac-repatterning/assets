72062c4fadceda7a5a85bb9dc8655b1fb5b38ccd
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-27 22:16:43 +0000

    Update src/cartography/interface.py: In Progress

--- src/cartography/interface.py
+++ src/cartography/interface.py
@@ -33,6 +33,8 @@ class Interface:
 
         # Instances
         self.__maps = src.acquire.maps.Maps(connector=self.__connector, s3_parameters=self.__s3_parameters)
+
+        # A `map tiles` option
         secret = src.functions.secret.Secret(connector=self.__connector)
         self.__tiles = secret.exc(secret_id=config.Config().project_key_name, node='background')
 
