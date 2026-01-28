85e396bd938b04f8cca0a4d26d703dd968d25754
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-28 01:27:27 +0000

    Update src/cartography/illustrate.py: In Progress

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -146,7 +146,9 @@ class Illustrate:
 
         folium.plugins.Draw(
             export=False, position='bottomleft', show_geometry_on_click=False,
-            draw_options={'polyline': False}).add_to(waves)
+            draw_options={'polyline': False, 'polygon': False, 'rectangle': False, 'marker': False,
+                          'circle': {'shapeOptions': {'color': 'orange', 'opacity': 0.25}}}
+        ).add_to(waves)
 
         # Persist
         outfile = os.path.join(__configurations.maps_, f'{_name}.html')
