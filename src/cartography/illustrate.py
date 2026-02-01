bf2fccb4cb39ccb71cf568ec578cc416ec5a3f79
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-01 21:20:20 +0000

    Update src/cartography/illustrate.py

--- src/cartography/illustrate.py
+++ src/cartography/illustrate.py
@@ -75,7 +75,8 @@ class Illustrate:
         # Base Layer
         waves = folium.Map(location=[self.__c_latitude, self.__c_longitude],
                            tiles=background.tiles, attr=background.attr,
-                           zoom_start=background.zoom_start, min_zoom=background.min_zoom, max_zoom=background.max_zoom)
+                           zoom_start=background.zoom_start, min_zoom=background.min_zoom, max_zoom=background.max_zoom,
+                           crs=background.crs)
         folium.GeoJson(
             data=self.__coarse.to_crs(epsg=3857),
             name='Boundaries',
