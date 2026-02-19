30928e867c8146d6a1ca1f666bce53a7e4627650
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-19 19:16:05 +0000

    Update src/cartography/backgrounds.py

--- src/cartography/backgrounds.py
+++ src/cartography/backgrounds.py
@@ -50,7 +50,10 @@ class Backgrounds:
              'filename': 'open-street-map', 'zoom_start': 7,  'min_zoom': 0, 'max_zoom': 19, 'crs': 'EPSG3857'},
             {'tiles': 'OpenStreetMap',
              'attr': None,
-             'filename': 'assets', 'zoom_start': 7, 'min_zoom': 0, 'max_zoom': 19, 'crs': 'EPSG3857'}
+             'filename': 'assets', 'zoom_start': 7, 'min_zoom': 0, 'max_zoom': 19, 'crs': 'EPSG3857'},
+            {'tiles': 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}.png',
+             'attr': 'Tiles &copy; Esri &mdash; Esri',
+             'filename': 'esri', 'zoom_start': 8, 'min_zoom': 8, 'max_zoom': 17, 'crs': 'EPSG3857'}
         ]
 
         return dictionary
