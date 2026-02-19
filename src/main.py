667ec8cdfb9df041ec2a5203e7c8df7fe14c2ea6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-19 18:53:29 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -18,10 +18,6 @@ def main():
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
 
-    # fetching the gauge station codes
-    # codes = src.sources.interface.Interface(
-    #     service=service, s3_parameters=s3_parameters, arguments=arguments).exc()
-
     # drawing
     src.cartography.interface.Interface(
         s3_parameters=s3_parameters, connector=connector).exc()
